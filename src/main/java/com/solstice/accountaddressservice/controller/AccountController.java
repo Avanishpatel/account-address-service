4f10e3325843285861b94021ef259303c3d35a5d
Avanish Patel <apatel@solstice.com>
2018-09-14 14:48:46 -0500

    Jenkins file test

--- src/main/java/com/solstice/accountaddressservice/controller/AccountController.java
+++ src/main/java/com/solstice/accountaddressservice/controller/AccountController.java
@@ -8,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
